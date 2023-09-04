5d1c0c79977accea00636a85558c2100793e4b85
Yahya Hamdy <Yahyahamdi650@gmail.com>
2023-09-04 22:05:13 +0000

    fix error

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -64,9 +64,11 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+        
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
+            'sexe' => $data['sexe'],
             'password' => Hash::make($data['password']),
         ]);
     }
